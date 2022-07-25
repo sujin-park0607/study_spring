224aa2e11bf1577b328d86c6938db4614e2c1658
sujin-park0607 <goweg0607@gmail.com>
2022-07-25 23:35:01 +0900

    add: Join test code

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -14,6 +14,7 @@ class MemberServiceTest {
     MemberService memberService = new MemberService();
 
     @Test
+//    void 회원가입()
     void join() {
         //given
         Member member = new Member();
@@ -26,6 +27,28 @@ class MemberServiceTest {
         Member findMember = memberService.findOne(saveId).get();
         assertThat(member.getName()).isEqualTo(findMember.getName());
 
+    }
+
+    @Test
+    public void 중복_회원_예외(){
+        //given
+        Member member1 = new Member();
+        member1.setName("Sujin");
+
+        Member member2 = new Member();
+        member2.setName("Sujin");
+
+        //when
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+            fail();
+        }catch (IllegalStateException e){
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }
+
+
+        //then
     }
 
     @Test
