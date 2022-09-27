3d50031cf4065aa3c11717c922f23c62f4afc7eb
sujin-park0607 <goweg0607@gmail.com>
2022-09-28 02:12:38 +0900

    add orderItemService

--- jpashop/src/main/java/jpabook/jpashop/domain/Item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.Item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,8 +27,27 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //== 비즈니스로직 ==//
 
+    /**
+     *
+     * Stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += 1;
+    }
 
+    /**
+     *
+     * Stock 감소
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
 
